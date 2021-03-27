5593603ef1011fe7a752aea87e7f659d48d354a7
Hadi374 <ahmadihadi374@gmail.com>
2021-03-27 23:03:34 +0430

    Add test code for express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Listening on port: " + port)
+})
