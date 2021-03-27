9b60ceaded877a2acf5fe3c350d40e0f1a132cf0
Hadi374 <ahmadihadi374@gmail.com>
2021-03-27 23:17:19 +0430

    test mysql connection

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const db = require('./util/database')
 
 const app = express();
+
+// add all routes here.
+
+
+app.get('/', (req, res) => {
+    db.execute('SELECT 2 + 2;')
+        .then(result => {
+            res.end(result)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    // res.end('hi')
+})
 
 
 app.use(bodyParser.urlencoded({extended: true}))
